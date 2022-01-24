695ee00ffe87afbb60efff58c808ec6d3eb4012a
EdgardGiorge <edgardgiorge@hotmail.com>
2022-01-23 23:21:18 -0300

    Automatiza div's e button

--- script.js
+++ script.js
@@ -1,104 +1,162 @@
+// Ref. Mentorias e salas de estudo e 3 code review. 19/01 requisito 4 16:30 e 5 Mentoria  16:40 Eduardo Bazler no PR da Mariana moraes. Exercícios do course
+
 function load() {
   const loading = document.querySelector('.loading');
-  return loading.remove();
+
+setTimeout(function () {
+  const body = document.querySelector('.body');
+  body.removeChild(loading);
+  }, 2000); // timout de 2 segundos pra carregar
 }
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-function createCustomElement(element, className, innerText, id = 0) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  e.id = id;
-  return e;
-}
+const img = document.createElement('img');
+img.className = 'item__image';
+img.src = imageSource;
+return img;
+} // buttons, img e descrições com os produtos visíveis
 
-let totalPrice = 0;
-function cartItemClickListener(event) {
-  const produto = event.target.innerText;
-  const idPreco = produto.indexOf('PRICE') + 8;
-  const preco = Number(produto.slice(idPreco, produto.length));
-  console.log(preco);
-  const preco1 = document.getElementById('price_cart');
-  totalPrice -= preco;
-  preco1.innerText = totalPrice;
-  event.target.parentNode.removeChild(event.target);
-  saveCartItems(event.target.id, false);
+function createCustomElement(element, className, innerText) {
+const e = document.createElement(element);
+e.className = className;
+e.innerText = innerText;
+return e;
 }
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  const preco = document.getElementById('price_cart');
-  totalPrice += salePrice;
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  preco.innerText = totalPrice;
-  li.id = sku;
-  saveCartItems(sku);
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+function createProductItemElement({ sku, name, image }) {
+const section = document.createElement('section');
+section.className = 'item';
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) { 
-  const section = document.createElement('section');
-  section.className = 'item';
+section.appendChild(createCustomElement('span', 'item__sku', sku));
+section.appendChild(createCustomElement('span', 'item__title', name));
+section.appendChild(createProductImageElement(image));
+section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  const button0 = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', sku);
-  button0.addEventListener('click', async (actualBtn) => {
-    const [car] = document.getElementsByClassName('cart__items');    
-    const id = await fetchItem(actualBtn.target.id);    
-    car.appendChild(createCartItemElement(id));  
-  });
-  section.appendChild(button0);
+document.getElementsByClassName('items')[0].appendChild(section);
 
-  return section;
+return section;
 }
 
 async function produtos() {
-  const produtos1 = await fetchProducts('computador');
-  load();
-  produtos1.results.forEach((element) => {
-    const items = document.getElementsByClassName('items');
-    items[0].appendChild(createProductItemElement(element));
+const produto = await fetchProducts('computador');
+produto.forEach((item) => {
+  const { id, title, thumbnail } = item;
+  const obj = { sku: id, name: title, image: thumbnail };
+  createProductItemElement(obj);
+});
+}
+
+const compras = document.querySelector('.cart__items');
+const div = document.querySelector('.total-price');
+
+function divPreco() {
+const pagar = document.createElement('p');
+pagar.innerText = '0';
+div.appendChild(pagar);
+}
+function totalPrice() {
+div.innerHTML = '';
+
+let sum = 0;
+const car = document.querySelectorAll('.cart__item');
+car.forEach((element) => {
+  const text = element.innerText;
+  const value = text.split('PRICE: $');
+  sum += Number(value[1]); 
+});
+
+const pagar = document.createElement('p');
+pagar.innerText = `${sum}`;
+div.appendChild(pagar);
+
+return sum;
+} // preço no cart somando e subtraindo
+
+let list = [];
+
+function cartStorage(objeto) {
+const item = JSON.stringify(objeto);
+saveCartItems(item);
+} // localStorage, armazenamento em list
+
+function cartItemClickListener(event) {
+const list1 = document.querySelectorAll('.cart__items');
+
+list1.forEach((item) => {
+    const text = event.target.innerText;
+    const value = text.split('PRICE: $');
+    const title = text.split('| NAME: ');
+    const title1 = title[1].split(' |');
+    const sku = title[0].split('SKU: ');
+    const skuOk = sku[1].split(' ');
+    const transformaTextoEmObj = { sku: skuOk[0], name: title1[0], salePrice: Number(value[1]) };
+    const result = list.filter((element) => element.sku !== transformaTextoEmObj.sku);
+    list = result;
+    cartStorage(list);
+    item.removeChild(event.target);
   });
-  return true;
-}
+} // localStorage, armazenamento em list
 
-function storage() {
-  const arrStorage = getSavedCartItems().filter((element) => element !== '');  
-  if (arrStorage.length > 0) {
-    arrStorage.forEach(async (element) => {
-      const [car] = document.getElementsByClassName('cart__items');    
-      const id = await fetchItem(element);          
-      car.appendChild(createCartItemElement(id)); 
-    });  
+function createCartItemElement({ sku, name, salePrice }) { 
+const li = document.createElement('li');
+li.className = 'cart__item';
+li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+
+document.getElementsByClassName('cart__items')[0].appendChild(li);
+
+totalPrice();
+li.addEventListener('click', cartItemClickListener);
+li.addEventListener('click', totalPrice);
+
+return li;
+} // inseri elementos no cart e traz preço Total
+
+async function produtoCarrinho(item) { 
+const obj = await fetchItem(item);
+const { id: identifica, title, price } = obj;
+const objEspecifico = { sku: identifica, name: title, salePrice: price };
+createCartItemElement(objEspecifico);
+list.push(objEspecifico);
+cartStorage(list);
+} // Cria lista pro localStorage
+
+function getStorageCart() { 
+if (localStorage.length > 0) {
+  const carrinho = JSON.parse(getSavedCartItems());
+  list = carrinho;
+  carrinho.forEach((item) => {
+     createCartItemElement(item);
+  });
+}
+} // lista de execução de compras no localStorage 
+
+function eventAddButton() { 
+  const button0 = document.querySelectorAll('.item__add');
+  
+  button0.forEach((botao, index) => {
+    botao.addEventListener('click', () => {
+      const items = document.getElementsByClassName('item__sku')[index].innerText;
+      produtoCarrinho(items); 
+    });
+  });
   }
-}
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function esvaziaCart() {
+const button1 = document.querySelector('.empty-cart');
+button1.addEventListener('click', () => {
+  compras.innerHTML = '';
+  const item = document.querySelector('.total-price>p');
+  item.innerText = '0';
+  localStorage.clear();
+  list = [];
+});
 }
+esvaziaCart();
 
-const [button1] = document.getElementsByClassName('empty-cart');
-async function esvaziaCarrinho() {
-  const cartItems = document.getElementsByClassName('cart__item');  
-  for (let i = 0; i < cartItems.length; i = 0) {
-    cartItems[i].parentNode.removeChild(cartItems[i]);
-  }
-  const preco = document.getElementById('price_cart');
-  preco.innerText = '';
-  localStorage.setItem('cartItems', '');
-}
-button1.addEventListener('click', esvaziaCarrinho);
-
-window.onload = () => { 
-  produtos();
-  storage();  
- };
+window.onload = async () => { 
+  load();
+  await produtos();
+  eventAddButton();
+  divPreco();
+  getStorageCart();
+};
