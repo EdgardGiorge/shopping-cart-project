6dd38b3dbc9317d9f47d521170228a8df9e90657
EdgardGiorge <edgardgiorge@hotmail.com>
2022-01-23 23:24:49 -0300

    Requisito 10

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,18 +1,7 @@
-let svId = '';
-const saveCartItems = (id, action = true) => {  
-  if (action) {
-    if (localStorage.getItem('cartItems') !== null) {
-      svId = localStorage.getItem('cartItems');
-    }
-    svId = `${svId} ${id}`;
-    svId = svId.trim();
-    localStorage.setItem('cartItems', svId);    
-  } else {
-    svId = svId.replace(id, '').trim();
-    localStorage.setItem('cartItems', svId);
-  }
+const saveCartItems = (listaItens) => {
+  localStorage.setItem('cartItems', listaItens);
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
+module.exports = saveCartItems;
 }
